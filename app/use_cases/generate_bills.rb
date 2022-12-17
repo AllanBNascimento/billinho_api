f128d4f0c3db8b438aa95af1b872b01aed217f98
AllanBNascimento <allannascimento68@gmail.com>
2022-12-17 02:33:12 -0300

    refactor use_case generate_bills

--- app/use_cases/generate_bills.rb
+++ app/use_cases/generate_bills.rb
@@ -8,43 +8,62 @@ class GenerateBills
   end
 
   def perform
-    first_due_date = calculate_first_due_date_to_date
-    amount, remainder = calculate_amounts
-    create_bills(first_due_date, amount, remainder)
+    calculate_first_due_date_to_date
+    calculate_bill_amount
+    calculate_remainder_value
+    create_bills
   end
 
   private
 
   def calculate_first_due_date_to_date
-    if enrollment.due_day < Date.today.day
-      (enrollment.due_day.to_s + Date.today.strftime('/%m/%Y')).to_date.next_month
-    elsif enrollment.due_day >= Date.today.end_of_month.day
-      Date.today.end_of_month
-    else
-      (enrollment.due_day.to_s + Date.today.strftime('/%m/%Y')).to_date
+    return mount_next_month_date if in_past?
+    return mount_end_of_month_date if end_of_month?
+    mount_month_date
+  end
+
+  def in_past?
+    enrollment.due_day < Date.today.day
+  end
+
+  def end_of_month?
+    enrollment.due_day >= Date.today.end_of_month.day
+  end
+
+  def mount_next_month_date
+    @first_due_date = (enrollment.due_day.to_s + Date.today.strftime('/%m/%Y')).to_date.next_month
+  end
+
+  def mount_end_of_month_date
+    @first_due_date = Date.today.end_of_month
+  end
+
+  def mount_month_date
+    @first_due_date = (enrollment.due_day.to_s + Date.today.strftime('/%m/%Y')).to_date
+  end
+
+  def calculate_bill_amount
+    @amount = enrollment.amount / enrollment.installments
+  end
+
+  def calculate_remainder_value
+    @remainder = enrollment.amount - (enrollment.installments * @amount)
+  end
+
+  def create_bills
+    @enrollment.installments.times do |installment|
+      bill_amount = installment < @remainder ? @amount + 1 : @amount
+      due_date = @first_due_date.next_month(installment)
+      create_bill(bill_amount, due_date)
     end
   end
 
-  def calculate_amounts
-    amount = enrollment.amount / enrollment.installments
-    remainder = enrollment.amount - (enrollment.installments * amount)
-    return amount, remainder
-  end
-
-  def create_bills(first_due_date, amount, remainder)
-    remaning = 1
-    for k in 1..@enrollment.installments do
-      if remainder > 0
-        remainder -= 1 
-      else
-        remaning = 0
-      end
-      Bill.create!(
-        amount: amount + remaning, 
-        due_date: first_due_date.next_month(k), 
-        status:'open', 
-        enrollment_id:enrollment.id
-      )
-    end
+  def create_bill(bill_amount, due_date)
+    Bill.create!(
+      amount: bill_amount, 
+      due_date: due_date, 
+      status:'open', 
+      enrollment_id:enrollment.id
+    )
   end
 end
